d6cd94ddb57bd1c5540ede753c1d325935ceb93a
EnzoSylvestrin <enzospavani@gmail.com>
2024-07-17 22:39:38 -0300

    fix: menu item route

--- src/app/api/menus/[menuId]/items/route.ts
+++ src/app/api/menus/[menuId]/items/route.ts
@@ -1,9 +1,11 @@
 import prisma from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: NextRequest, { params }: { params: any }) {
-    const { menuId } = params;
-    const { name, quantity, price } = await req.json();
+export async function POST(req: NextRequest, { params } : { params: { menuId: string } }) {
+  const { menuId } = params;
+  const { name, quantity, price } = await req.json();
+  
+  try {
     const item = await prisma.item.create({
       data: {
         name,
@@ -13,4 +15,7 @@ export async function POST(req: NextRequest, { params }: { params: any }) {
       },
     });
     return NextResponse.json(item);
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
