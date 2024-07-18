80961ed947576f96d5bbd5b3e3bbfb1d51255700
EnzoSylvestrin <enzospavani@gmail.com>
2024-07-17 22:19:45 -0300

    fix: change bcrypt to bcryptjs

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '../../../lib/prisma';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { z } from 'zod';
 
 const userSchema = z.object({
