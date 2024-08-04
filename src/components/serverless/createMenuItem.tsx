637312fc705ed0717e5e8ada58db95d7f20cd1f7
EnzoSylvestrin <enzospavani@gmail.com>
2024-08-04 05:55:10 -0300

    fix: menu.item does not exist

--- src/components/serverless/createMenuItem.tsx
+++ src/components/serverless/createMenuItem.tsx
@@ -4,14 +4,14 @@ import prisma from "@/lib/prisma";
 
 export async function createMenuItem(menuId: any, item: { name: any; quantity: any; price: any; }) {
 
-    const createdItem = await prisma.item.create({
-        data: {
-            name: item.name,
-            quantity: item.quantity,
-            price: item.price,
-            menuId: parseInt(menuId, 10),
-        },
-    });
+    // const createdItem = await prisma.item.create({
+    //     data: {
+    //         name: item.name,
+    //         quantity: item.quantity,
+    //         price: item.price,
+    //         menuId: parseInt(menuId, 10),
+    //     },
+    // });
 
-    return createdItem;
+    // return createdItem;
 }
