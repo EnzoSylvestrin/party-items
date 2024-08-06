1746da4f74324eb79c9720b1298a282d4d0219a0
EnzoSylvestrin <enzospavani@gmail.com>
2024-08-05 21:33:33 -0300

    feat: adding toogleBar to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
+'use client';
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./styles/globals.css";
 import { AuthProvider } from "@/context/AuthContext";
+import ToggleBar from "@/components/menu/ToggleBar";
+import { getMenus } from "@/components/serverless/getMenus";
+import { Menu } from "@prisma/client";
+import { useState, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,10 +21,27 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [menus, setMenus] = useState<Menu[]>([]);
+
+  async function loadMenus() {
+    let menus : any = await getMenus();
+
+    setMenus(menus);
+  }
+
+  useEffect(() => {
+    loadMenus();
+  }, []);
+
   return (
     <html lang="en">
       <AuthProvider>
-        <body className={`min-h-screen bg-zinc-800 text-text ${inter.className}`}>{children}</body>
+        <body className={`min-h-screen bg-zinc-800 text-text ${inter.className}`}>
+          <ToggleBar menus={menus} refresh={loadMenus} />
+          <div className='ml-60'>
+            {children}
+          </div>
+        </body>
       </AuthProvider>
     </html>
   );
--- src/app/menu/[menu]/page.tsx
+++ src/app/menu/[menu]/page.tsx
@@ -6,18 +6,16 @@ import { useRouter } from 'next/navigation';
 
 import Swal from 'sweetalert2';
 
-import ToggleBar from '@/components/menu/ToggleBar';
 import { NewColumn } from '@/components/menu/NewColumn';
 import { NewRow } from '@/components/menu/NewRow';
 
-import { getMenus } from '@/components/serverless/getMenus';
 import { getMenuData } from '@/components/serverless/getMenuData';
 
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
 import { useAuth } from '@/context/AuthContext';
 
-import { Menu, Prisma, User } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
 
 import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card';
 
@@ -32,7 +30,6 @@ export type TableDataType = {
 }
 
 export default function MenuPage({ params }: { params: any }) {
-  const [menus, setMenus] = useState<Menu[]>([]);
   const [menu, setMenu] = useState<any | null>(null);
   const [tableData, setTableData] = useState<TableDataType>({ columns: null, rows: [] });
 
@@ -43,11 +40,6 @@ export default function MenuPage({ params }: { params: any }) {
 
   const auth = useAuth();
   const isUserLogged = auth?.isUserLogged();
-
-  async function loadMenus() {
-    let menus = await getMenus();
-    setMenus(menus);
-  }
 
   async function loadMenu(menuId: string) {
     const menu = await getMenuData(menuId);
@@ -70,7 +62,6 @@ export default function MenuPage({ params }: { params: any }) {
   }
 
   useEffect(() => {
-    loadMenus();
     loadMenu(params.menu);
   }, [params.menu]);
 
@@ -123,47 +114,44 @@ export default function MenuPage({ params }: { params: any }) {
 
   return (
     <>
-      <ToggleBar menus={menus} refresh={loadMenus} />
-      <div className='ml-60'>
-        <div className='w-full h-[100vh] flex flex-col justify-center items-center gap-6 p-16'>
-          <h1 className='text-5xl text-start text-white self-start'>{menu?.name}</h1>
-          <Table>
-            <TableCaption className='text-white'>Adicione campos ou linhas a essa tabela!</TableCaption>
-            <TableHeader>
-              <TableRow className='hover:bg-transparent'>
-                {tableData.columns && Object.keys(tableData.columns).map((column, index) => (
-                  <TableHead key={index}>{(tableData.columns as { [key: string]: string })[column]}</TableHead>
-                ))}
-                {(tableData.columns && Object.keys(tableData.columns).length < 6) && <TableHead className='cursor-pointer' onClick={handleOpenColumnDialog}>+</TableHead>}
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {tableData.rows.map((row, rowIndex) => (
-                <HoverCard key={rowIndex}>
-                  <HoverCardTrigger asChild>
-                    <TableRow>
-                      {tableData.columns && Object.values(tableData.columns).map((column, colIndex) => (
-                        <TableCell key={colIndex}>{row.data[column]}</TableCell>
-                      ))}
-                      {(tableData.columns && Object.keys(tableData.columns).length < 6) && <TableCell></TableCell>}
-                    </TableRow>
-                  </HoverCardTrigger>
-                  <HoverCardContent className='w-80'>
-                    <div className='rounded-lg flex items-center justify-between'>
-                      <p className='text-sm'>Criado por: <span style={{ color: row.user.color }}>{row.user.name}</span></p>
-                    </div>
-                  </HoverCardContent>
-                </HoverCard>
+      <div className='w-full h-[100vh] flex flex-col justify-center items-center gap-6 p-16'>
+        <h1 className='text-5xl text-start text-white self-start'>{menu?.name}</h1>
+        <Table>
+          <TableCaption className='text-white'>Adicione campos ou linhas a essa tabela!</TableCaption>
+          <TableHeader>
+            <TableRow className='hover:bg-transparent'>
+              {tableData.columns && Object.keys(tableData.columns).map((column, index) => (
+                <TableHead key={index}>{(tableData.columns as { [key: string]: string })[column]}</TableHead>
               ))}
-              {
-                tableData.columns && Object.keys(tableData.columns).length > 0 &&
-                <TableRow>
-                  <TableCell colSpan={Object.keys(tableData.columns ?? {}).length + 1} className='cursor-pointer' onClick={handleOpenRowDialog}>+</TableCell>
-                </TableRow>
-              }
-            </TableBody>
-          </Table>
-        </div>
+              {(tableData.columns && Object.keys(tableData.columns).length < 6) && <TableHead className='cursor-pointer' onClick={handleOpenColumnDialog}>+</TableHead>}
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tableData.rows.map((row, rowIndex) => (
+              <HoverCard key={rowIndex}>
+                <HoverCardTrigger asChild>
+                  <TableRow>
+                    {tableData.columns && Object.values(tableData.columns).map((column, colIndex) => (
+                      <TableCell key={colIndex}>{row.data[column]}</TableCell>
+                    ))}
+                    {(tableData.columns && Object.keys(tableData.columns).length < 6) && <TableCell></TableCell>}
+                  </TableRow>
+                </HoverCardTrigger>
+                <HoverCardContent className='w-80'>
+                  <div className='rounded-lg flex items-center justify-between'>
+                    <p className='text-sm'>Criado por: <span style={{ color: row.user.color }}>{row.user.name}</span></p>
+                  </div>
+                </HoverCardContent>
+              </HoverCard>
+            ))}
+            {
+              tableData.columns && Object.keys(tableData.columns).length > 0 &&
+              <TableRow>
+                <TableCell colSpan={Object.keys(tableData.columns ?? {}).length + 1} className='cursor-pointer' onClick={handleOpenRowDialog}>+</TableCell>
+              </TableRow>
+            }
+          </TableBody>
+        </Table>
       </div>
       {openColumnDialog && ( 
         <NewColumn open={openColumnDialog} setOpen={setOpenColumnDialog} handleAddField={handleAddField} menuId={params.menu} />
